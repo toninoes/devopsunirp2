f2e6a5b5d8008d6c1e61f7c5362bdb3b9a826f4e
Antonio Ruiz <toninoes@gmail.com>
2021-07-02 19:49:36 +0200

    Creados playbooks/roles para all y nfs-server

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
-
+ansible-playbook -i hosts 01-todos_hosts.yaml
+ansible-playbook -i hosts --limit nfs 02-servidor_nfs.yaml

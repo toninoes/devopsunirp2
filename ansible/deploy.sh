0cbf1e93c87ba9ee5480063f4fbdef6f3da89903
Antonio Ruiz <toninoes@gmail.com>
2021-07-04 20:14:18 +0200

    Creados playbooks/roles para master-workers

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,4 +2,5 @@
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
 ansible-playbook -i hosts 01-todos_hosts.yaml
-ansible-playbook -i hosts --limit nfs 02-servidor_nfs.yaml
+ansible-playbook -i hosts 02-servidor_nfs.yaml
+ansible-playbook -i hosts 03-master_workers.yaml

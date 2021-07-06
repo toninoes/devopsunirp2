7e4230796201e2075cf08f04618c3b4fc2eb3cc3
Antonio Ruiz <toninoes@gmail.com>
2021-07-06 21:06:06 +0200

    Creados playbooks/roles para master y tb para workers. Falta ultimo paso, no soy capaz de unir al cluster

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -4,3 +4,4 @@
 ansible-playbook -i hosts 01-todos_hosts.yaml
 ansible-playbook -i hosts 02-servidor_nfs.yaml
 ansible-playbook -i hosts 03-master_workers.yaml
+ansible-playbook -i hosts 04-master.yaml

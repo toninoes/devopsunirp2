1fbe3cd6bd0bc86dd9a61232e976e8f6da84e949
Antonio Ruiz <toninoes@gmail.com>
2021-07-06 21:08:26 +0200

    Añadido playbook al deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -5,3 +5,4 @@ ansible-playbook -i hosts 01-todos_hosts.yaml
 ansible-playbook -i hosts 02-servidor_nfs.yaml
 ansible-playbook -i hosts 03-master_workers.yaml
 ansible-playbook -i hosts 04-master.yaml
+ansible-playbook -i hosts 05-workers.yaml

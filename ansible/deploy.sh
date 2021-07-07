ee87901d7c6b49bf6fe4bf379171732082af705d
Antonio Ruiz <toninoes@gmail.com>
2021-07-07 20:29:10 +0200

    Solucionado la union de los workers al cluster

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -6,3 +6,5 @@ ansible-playbook -i hosts 02-servidor_nfs.yaml
 ansible-playbook -i hosts 03-master_workers.yaml
 ansible-playbook -i hosts 04-master.yaml
 ansible-playbook -i hosts 05-workers.yaml
+ansible-playbook -i hosts 06-guardar_token_master.yaml
+ansible-playbook -i hosts 07-unir_workers_cluster.yaml

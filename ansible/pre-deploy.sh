432c9f20e117c88cd6ef39aad105d98c690bea83
Antonio Ruiz <toninoes@gmail.com>
2021-07-08 18:38:49 +0200

    Añadido pre-deply script, para configurar master desde equipo local

---
+++ ansible/pre-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" != "" ]; then
+    scp ~/.ssh/id_rsa ~/.ssh/id_rsa.pub  adminUsername@$1:~/.ssh
+	ssh adminUsername@$1 '''
+	sudo yum install epel-release -y
+	sudo yum install ansible git -y
+	sudo sed '/host_key_checking/s/^#//g' -i /etc/ansible/ansible.cfg
+	git clone https://github.com/toninoes/devopsunirp2.git
+	devopsunirp2/ansible/deploy.sh
+	'''
+else
+    echo "Necesito IP pública de master como argumento de este script. Ejecute de nuevo."
+fi
